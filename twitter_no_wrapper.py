3da258d2620528e9fd471d21f226699316252953
poptart-sommelier <rich.d.nospam@gmail.com>
2018-06-20 22:48:15 -0400

    stuff from todo has been done

--- twitter_no_wrapper.py
+++ twitter_no_wrapper.py
@@ -4,9 +4,12 @@ import requests
 import json
 import urllib3
 from requests_oauthlib import OAuth1
+import operator
+import datetime
+import collections
 
+# TODO: Add logging
 # TODO: THREAD URL UNSHORTEN (TEST LEN FIRST WITH urllib3 to reduce lookups)
-# TODO: API CALLS SHOULD HAVE A PARAMETER THAT LIMITS THE NUMBER OF CALLS (FOR TESTING). I.E DON'T LOOP UNTIL THROTTLED
 
 NUMBER_OF_ITEMS = 200
 CRED_PATH = '/home/rich/.creds/twitter_api.json'
@@ -23,6 +26,8 @@ ACCESS_TOKEN_SECRET = creds['twitter_creds'][0]['ACCESS_TOKEN_SECRET']
 
 AUTH = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
+JSON_OUTPUT_DIR = './'
+
 HOME_TIMELINE_URL = 'https://api.twitter.com/1.1/statuses/home_timeline.json?tweet_mode=extended&count=200&include_rts=True&include_entities=True'
 
 def parse_retweet(json_response):
@@ -34,8 +39,28 @@ def parse_retweet(json_response):
 			'user': json_response['retweeted_status']['user']['name'],
 			'full_text': json_response['retweeted_status']['full_text'],
 			'id': json_response['retweeted_status']['id_str'],
-			'tweet_direct_link': 'https://twitter.com/' + json_response['retweeted_status']['screen_name'] + '/status/' + json_response['retweeted_status']['id_str'],
-			'urls': urls
+			'tweet_direct_link': 'https://twitter.com/' + json_response['retweeted_status']['user']['screen_name'] + '/status/' + json_response['retweeted_status']['id_str'],
+			'urls': urls,
+			'tweet_type': 'retweet'
+		}
+		return r_dict
+
+	else:
+		return None
+
+
+def parse_quoted_tweet(json_response):
+	urls = [url['expanded_url'] for url in json_response['quoted_status']['entities']['urls']]
+
+	if len(urls) > 0:
+		r_dict = {
+			'screen_name': json_response['quoted_status']['user']['screen_name'],
+			'user': json_response['quoted_status']['user']['name'],
+			'full_text': json_response['quoted_status']['full_text'],
+			'id': json_response['quoted_status']['id_str'],
+			'tweet_direct_link': 'https://twitter.com/' + json_response['quoted_status']['user']['screen_name'] + '/status/' + json_response['quoted_status']['id_str'],
+			'urls': urls,
+			'tweet_type': 'quoted tweet'
 		}
 		return r_dict
 
@@ -53,7 +78,8 @@ def parse_tweet(json_response):
 			'full_text': json_response['full_text'],
 			'id': json_response['id_str'],
 			'tweet_direct_link': 'https://twitter.com/' + json_response['user']['screen_name'] + '/status/' + json_response['id_str'],
-			'urls': urls
+			'urls': urls,
+			'tweet_type': 'standard tweet'
 		}
 		return r_dict
 
@@ -61,12 +87,20 @@ def parse_tweet(json_response):
 		return None
 
 
+def write_json_output(_all_tweets):
+
+	json_output = JSON_OUTPUT_DIR + "json_output_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%I%M%S")) + ".json"
+
+	with open(json_output, 'w') as j:
+		json.dump(_all_tweets, j)
+
+
 def get_last_tweet_id():
 	try:
 		with open(LAST_ACCESSED_FILE, 'r') as f:
 			last_read = f.read()
 			if last_read.isdigit():
-				return last_read
+				return int(last_read)
 			else:
 				return 1
 
@@ -91,14 +125,27 @@ def set_last_tweet_id(last_write):
 
 def is_shortened(url):
 	# shortened paths look like this:
-	# https://bit.ly/AjIOdjl/
+	# https://bit.ly/AjIOdjl
 	# use urllib to measure the following:
 	# host length
 	# does it have a path
 	# how long is the path (shortened paths only have 6-8 characters-ish)
 	# does the path have more than one '/'
 	# we can eliminate a lot of urls that way, then unshorten the rest
-	pass
+	parsed_url = urllib3.util.parse_url(url)
+
+	cntr = collections.Counter(parsed_url.path)
+
+	if cntr['/'] > 1:
+		return False
+	if 'sans.org/u/' not in url:
+		return None
+	if len(parsed_url.host) > 12:
+		return False
+	if parsed_url.path and len(parsed_url.path) > 12:
+		return False
+	if parsed_url.path == '/' or parsed_url.path == None:
+		return False
 
 
 def unshorten_url(url):
@@ -116,9 +163,9 @@ def get_tweets(since_id=1, max_id=None):
 
 	r = requests.get(req_path, auth=AUTH)
 
-	if r.status_code == 200:
-		json_resp = r.json()
+	json_resp = r.json()
 
+	if (r.status_code == 200) and (len(json_resp) > 0):
 		first_last_ids = {
 			'latest_id': max([j['id'] for j in r.json()]),
 			'oldest_id': min([j['id'] for j in r.json()])
@@ -130,10 +177,15 @@ def get_tweets(since_id=1, max_id=None):
 		for jr in json_resp:
 			# Is a retweet
 			if 'retweeted_status' in jr:
-				t = parse_tweet(jr)
+				t = parse_retweet(jr)
 				if t:
 					tweets.append(t)
-			# Is not a retweet
+			# Is a quoted tweet
+			elif 'quoted_status' in jr:
+				t = parse_quoted_tweet(jr)
+				if t:
+					tweets.append(t)
+			# Is standard tweet
 			else:
 				t = parse_tweet(jr)
 				if t:
@@ -144,31 +196,43 @@ def get_tweets(since_id=1, max_id=None):
 	else:
 		return None, None, None
 
-all_tweets = []
+def go(api_calls):
+	all_tweets = []
+	temp_tweets = []
 
-temp_tweets = []
+	last_tweet_id = get_last_tweet_id()
 
-last_tweet_id = get_last_tweet_id()
+	temp_tweets, first_last, status_code = get_tweets(last_tweet_id)
 
-temp_tweets, first_last, status_code = get_tweets(last_tweet_id)
+	if (temp_tweets):
+		last_tweet_id_new = first_last['latest_id']
+		all_tweets.extend(temp_tweets)
+		# use 'while' for normal function
+		# while True:
+		# use 'for' to restrict number of api calls.
+		if (last_tweet_id != first_last['oldest_id'] - 1):
+			for i in range(api_calls):
+				temp_tweets, first_last, status_code = get_tweets(last_tweet_id, first_last['oldest_id'])
+				# while since_id < max_id - 1
+				if status_code == 200:
+					if first_last['latest_id'] - 1 > int(last_tweet_id):
+						all_tweets.extend(temp_tweets)
+					else:
+						break
+				else:
+					break
 
-all_tweets.extend(temp_tweets)
+		set_last_tweet_id(last_tweet_id_new)
 
-if status_code == 200:
-# set_last_tweet_id(first_last['latest_id'])
-	# use 'while' for normal function
-	# while True:
-	# use 'for' to restrict number of api calls.
-	for i in range(2):
-		temp_tweets, first_last, status_code = get_tweets(last_tweet_id, first_last['oldest_id'])
-		# while since_id < max_id - 1
-		if status_code == 200:
-			if first_last['latest_id'] - 1 > int(last_tweet_id):
-				all_tweets.extend(temp_tweets)
-			else:
-				break
-		else:
-			set_last_tweet_id(first_last['latest_id'])
-			break
+	all_tweets.sort(key=operator.itemgetter('id'))
 
-print(json.dumps(all_tweets))
+#	print(json.dumps(all_tweets))
+
+	for x in all_tweets:
+		for u in x['urls']:
+			print(u)
+			print(is_shortened(u))
+
+	write_json_output(all_tweets)
+
+go(0)
