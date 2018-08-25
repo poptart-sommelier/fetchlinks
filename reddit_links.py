bc6001514517216bbc3f29e46710c4e669969ee1
poptart-sommelier <rich.d.nospam@gmail.com>
2018-08-25 00:09:00 -0400

    more stuff

--- reddit_links.py
+++ reddit_links.py
@@ -9,11 +9,10 @@ creds = json.loads(json_data)
 APP_CLIENT_ID = creds['reddit_creds']['APP_CLIENT_ID']
 APP_CLIENT_SECRET = creds['reddit_creds']['APP_CLIENT_SECRET']
 
-SUBREDDITS = ['Netsec']
+SUBREDDITS = ['Netsec', 'Malware', 'Antiforensics', 'Computerforensics', 'ReverseEngineering']
 QUERY_PART1 = 'https://oauth.reddit.com/r/'
-QUERY_PART2 = '/.json'
+QUERY_PART2 = '/new/.json'
 USER_AGENT = 'Get_Links Agent'
-PARAMS = {'sort': 'top', 'show': 'all', 't': 'all'}
 
 
 def auth():
@@ -31,25 +30,48 @@ def make_request(reddit, token, after=None):
     # Build the URL
     if not after:
         url = QUERY_PART1 + reddit + QUERY_PART2
+        params = {'sort': 'new', 'show': 'all', 't': 'all', 'limit': '25'}
     else:
         url = QUERY_PART1 + reddit + QUERY_PART2
-        PARAMS['after'] = after
+        params = {'sort': 'new', 'show': 'all', 't': 'all', 'limit': '25', 'after': after}
 
-    api_res = requests.get(url=url, params=PARAMS, headers={'authorization': 'Bearer ' + token, 'User-agent': USER_AGENT})
+    api_res = requests.get(url=url, params=params, headers={'authorization': 'Bearer ' + token, 'User-agent': USER_AGENT})
 
-    # Now for a regular API request (but to oauth.reddit.com)
-    # api_res = requests.get('https://oauth.reddit.com/r/redditdev/comments/61noov/do_you_have_to_authenticate_even_if_only_using/.json',
-    #                        headers={'authorization': 'Bearer ' + token, 'User-agent': USER_AGENT})
+    new_posts = api_res.json()
 
-    post_listing = api_res.json()
-    post = post_listing['data']['children'][0]
-    title = post['data']['title']
+    return new_posts
+
+# TODO: Parse 'selftext' for urls and add them. condition should be if not reddit and selftext_urls = None
+def parse_json(json_response):
+    if not json_response['data']['url'].startswith('https://www.reddit.com/'):
+        r_dict = {
+            'user': json_response['data']['author'],
+            'title': json_response['data']['title'],
+            'id': json_response['data']['id'],
+            'direct_link': 'https://www.reddit.com' + json_response['data']['permalink'],
+            'url': json_response['data']['url'],
+            'subreddit': json_response['data']['subreddit_name_prefixed'],
+            'date_created': json_response['data']['created_utc']
+        }
+
+        return r_dict
+    else:
+        return None
 
 
-def parse_json():
-    pass
-
+all_posts = []
 
 token = auth()
 
-make_request('Netsec', token)
+for sr in SUBREDDITS:
+    resp = make_request(sr, token)
+    print(json.dumps(resp))
+
+    for r in resp['data']['children']:
+        post = parse_json(r)
+        if post:
+            all_posts.append(post)
+
+
+
+print(json.dumps(all_posts))
